c0c0f332405dc30b8c1e3bc575e6e3fe6e49ab87
Sergey Koshechkin <serge.koshechkin@gmail.com>
2016-12-21 01:21:35 +0300

    add rzslider implementetion

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ npm install
 cp node_modules/angular/angular.min.js libs/angular.min.js
 cp node_modules/ngmap/build/scripts/ng-map.min.js libs/ng-map.min.js
 cp node_modules/js-marker-clusterer/src/markerclusterer_compiled.js libs/markerclusterer.min.js
+cp node_modules/angularjs-slider/dist/rzslider.min.css libs/rzslider.min.css
+cp node_modules/angularjs-slider/dist/rzslider.min.js libs/rzslider.min.js
--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  */
 (function(angular) {
     "use strict";
-    var app = angular.module('FileClustering', ['ngMap']);
+    var app = angular.module('FileClustering', ['ngMap', 'rzModule']);
 
     app.controller('MapController', ['NgMap', '$scope', '$rootScope', '$http',
         function(NgMap, $scope, $rootScope, $http) {
@@ -42,5 +42,21 @@
                 $scope.map.setCenter($scope.bounds.getCenter());
                 $scope.map.fitBounds($scope.bounds);
             });
+            /* Slider initialization */
+            var dates = [];
+            for (var i = 1; i <= 31; i++) {
+                dates.push(new Date(2016, 7, i));
+            }
+            $scope.slider = {
+                value: dates[0], // or new Date(2016, 7, 10) is you want to use different instances
+                options: {
+                    stepsArray: dates,
+                    translate: function (date) {
+                        if (date != null)
+                            return date.toDateString();
+                        return '';
+                    }
+                }
+            };
     }]);
 }) (angular);
