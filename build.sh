4b3fc81e3f543c741b20c0e2003a85bd3f64e8df
Sergey Koshechkin <serge.koshechkin@gmail.com>
2016-12-20 04:07:12 +0300

    add markerclustener

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 npm install
 cp node_modules/angular/angular.min.js libs/angular.min.js
 cp node_modules/ngmap/build/scripts/ng-map.min.js libs/ng-map.min.js
+cp node_modules/js-marker-clusterer/src/markerclusterer_compiled.js libs/markerclusterer.min.js
