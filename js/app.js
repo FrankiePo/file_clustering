4415783d99de7df6ceb2a93987594df68e830276
Sergey Koshechkin <serge.koshechkin@gmail.com>
2016-12-20 04:07:46 +0300

    implement simple markerclustener

--- js/app.js
+++ js/app.js
@@ -20,5 +20,23 @@
                 });
             };
             $scope.load_items();
+            $scope.createMarkerForItem = function (item) {
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(item.geo[0], item.geo[1]),
+                    title: item.file
+                });
+                return marker;
+            };
+            $scope.initMarkerClusterer = function () {
+                var markers = $rootScope.items.map(function (city) {
+                    return $scope.createMarkerForItem(city);
+                });
+                var mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
+                return new MarkerClusterer($scope.map, markers, mcOptions);
+            };
+            NgMap.getMap().then(function(map) {
+                $scope.map = map;
+                $scope.initMarkerClusterer();
+            });
     }]);
 }) (angular);
