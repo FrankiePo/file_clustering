9662e984e97605a981f4df4974855cde8a5a9b24
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-03-05 12:19:49 +0300

    add search input + refactor a bit

--- js/app.js
+++ js/app.js
@@ -3,25 +3,23 @@
  */
 (function(angular) {
     "use strict";
-    var app = angular.module('FileClustering', ['ngMap']);
+    const app = angular.module('FileClustering', ['ngMap']);
 
     app.controller('MapController', ['NgMap', '$scope', '$rootScope', '$http',
         function(NgMap, $scope, $rootScope, $http) {
             $scope.load_items = function () {
-                var request = {
+                const request = {
                     url: 'htc_one_data.json',
                     method: "GET"
                 };
                 $http(request).then(function(res) {
-                    $rootScope.items = res.data.items.filter(function(item) {
-                        return item.geo;
-                    });
+                    $rootScope.items = res.data.items.filter((item) => item.geo);
                     $rootScope.titles = res.data.titles;
                 });
             };
             $scope.load_items();
             $scope.createMarkerForItem = function (item) {
-                var marker = new google.maps.Marker({
+                let marker = new google.maps.Marker({
                     position: new google.maps.LatLng(item.geo[0], item.geo[1]),
                     title: item.file
                 });
@@ -29,10 +27,8 @@
                 return marker;
             };
             $scope.initMarkerClusterer = function () {
-                var markers = $rootScope.items.map(function (city) {
-                    return $scope.createMarkerForItem(city);
-                });
-                var mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
+                let markers = $rootScope.items.map((city) => $scope.createMarkerForItem(city));
+                let mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
                 return new MarkerClusterer($scope.map, markers, mcOptions);
             };
             NgMap.getMap().then(function(map) {
