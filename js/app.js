2fa119c8204e23de7709ce54f1e50a7616132431
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-03-21 21:03:29 +0300

    app: add clusterisation with search

--- js/app.js
+++ js/app.js
@@ -4,39 +4,41 @@
 (function(angular) {
     "use strict";
     const app = angular.module('FileClustering', ['ngMap']);
-
-    app.controller('MapController', ['NgMap', '$scope', '$rootScope', '$http',
-        function(NgMap, $scope, $rootScope, $http) {
-            $scope.load_items = function () {
+    app.controller('MapController', ['NgMap', '$scope', '$http', "$filter",
+        function(NgMap, $scope, $http) {
+            (function() {
                 const request = {
                     url: 'htc_one_data.json',
                     method: "GET"
                 };
                 $http(request).then(function(res) {
-                    $rootScope.items = res.data.items.filter((item) => item.geo);
-                    $rootScope.titles = res.data.titles;
+                    $scope.items = res.data.items;
+                    $scope.titles = res.data.titles;
                 });
-            };
-            $scope.load_items();
-            $scope.createMarkerForItem = function (item) {
-                let marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(item.geo[0], item.geo[1]),
-                    title: item.file
-                });
-                $scope.bounds.extend(marker.getPosition());
-                return marker;
-            };
-            $scope.initMarkerClusterer = function () {
-                let markers = $rootScope.items.map((city) => $scope.createMarkerForItem(city));
-                let mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
-                return new MarkerClusterer($scope.map, markers, mcOptions);
-            };
+            }) ();
             NgMap.getMap().then(function(map) {
                 $scope.map = map;
-                $scope.bounds = new google.maps.LatLngBounds();
-                $scope.initMarkerClusterer();
-                $scope.map.setCenter($scope.bounds.getCenter());
-                $scope.map.fitBounds($scope.bounds);
+                const mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
+                $scope.markerClusterer = new MarkerClusterer(map, [], mcOptions);
+                $scope.$watchGroup(['items', 'searchString'], function (newValues, oldValues, scope) {
+                    const withGeo = item => item.geo;
+                    const withString = item => (!$scope.searchString) ? true : item.file.includes($scope.searchString);
+                    const createMarker = item => new google.maps.Marker({
+                        position: new google.maps.LatLng(item.geo[0], item.geo[1]),
+                        title: item.file,
+                        icon: `images/thumbnails/thumbnail_50_50_${item.file}`
+                    });
+                    scope.markers = scope.items && scope.items.filter(withGeo).filter(withString).map(createMarker);
+                });
+                $scope.$watch('markers', function(newValues, oldValues, scope) {
+                    if (!scope.markers) return;
+                    const mc = scope.markerClusterer;
+                    mc.clearMarkers();
+                    mc.addMarkers(scope.markers);
+                    mc.fitMapToMarkers();
+                });
             });
-    }]);
+
+
+        }]);
 }) (angular);
