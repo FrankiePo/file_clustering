dff59f311c6bca756e592c39ef6ed1c8174442e1
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-03-21 22:40:38 +0300

    app: add constant MapCfg

--- js/app.js
+++ js/app.js
@@ -11,13 +11,18 @@
             getTitles: () => request.then(res => res.data.titles)
         }
     }]);
-    app.controller('MapController', ['NgMap', 'Items', '$scope',
-        function(NgMap, Items, $scope) {
+    app.constant('MapCfg', {
+        mcOptions: {
+            imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m'
+        },
+        thumbnailsPath: (filename) => `images/thumbnails/thumbnail_50_50_${filename}`,
+    });
+    app.controller('MapController', ['NgMap', 'Items', 'MapCfg', '$scope',
+        function(NgMap, Items, MapCfg, $scope) {
             Items.getItems().then(items => $scope.items = items);
             // Items.getTitles().then(titles => $scope.titles = titles);
             NgMap.getMap().then(function(map) {
-                const mcOptions = { imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' };
-                $scope.markerClusterer = new MarkerClusterer(map, [], mcOptions);
+                $scope.markerClusterer = new MarkerClusterer(map, [], MapCfg.mcOptions);
                 $scope.$watchGroup(['items', 'searchString'], function (newValues, oldValues, scope) {
                     if (!scope.items) return;
                     const withGeo = item => item.geo;
@@ -25,12 +30,12 @@
                     const createMarker = item => new google.maps.Marker({
                         position: new google.maps.LatLng(item.geo[0], item.geo[1]),
                         title: item.file,
-                        icon: `images/thumbnails/thumbnail_50_50_${item.file}`
+                        icon: MapCfg.thumbnailsPath(item.file),
                     });
                     scope.markers = scope.items
-                        .filter(withGeo).
-                        filter(withString).
-                        map(createMarker);
+                        .filter(withGeo)
+                        .filter(withString)
+                        .map(createMarker);
                 });
                 $scope.$watch('markers', function(newValues, oldValues, scope) {
                     if (!scope.markers) return;
@@ -40,7 +45,5 @@
                     mc.fitMapToMarkers();
                 });
             });
-
-
         }]);
 }) (angular);
